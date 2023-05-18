9c31586f1732aa3b42bb273e373dc70d1ee87e3d
hafiz1379 <hafizrasa1379@gmail.com>
2023-05-18 17:11:56 +0430

    Add clear all completed test

--- src/modules/functions.test.js
+++ src/modules/functions.test.js
@@ -1,4 +1,6 @@
-import { add, remove } from './functions.js';
+import {
+  add, remove, complete, edit, clearCompleted,
+} from './functions.js';
 
 global.localStorage = {
   getItem: jest.fn(),
@@ -7,7 +9,7 @@ global.localStorage = {
 };
 
 describe('add function', () => {
-  test('adds a item to the tasks array', () => {
+  test('adds one item to the tasks array', () => {
     const tasks = [];
     add(tasks, 'New task');
     expect(tasks.length).toBe(1);
@@ -25,7 +27,7 @@ describe('add function', () => {
 });
 
 describe('remove function', () => {
-  test('removes a item from the tasks array', () => {
+  test('removes one item from the tasks array', () => {
     const tasks = [
       { description: 'Task 1', completed: false, index: 1 },
       { description: 'Task 2', completed: false, index: 2 },
@@ -63,5 +65,72 @@ describe('remove function', () => {
 
     expect(tasks[0].index).toBe(1);
     expect(tasks[1].index).toBe(2);
+  });
+});
+describe('complete function', () => {
+  test('completes a task that is not already completed', () => {
+    const tasks = [
+      { description: 'Task 1', completed: false, index: 1 },
+      { description: 'Task 2', completed: false, index: 2 },
+      { description: 'Task 3', completed: false, index: 3 },
+    ];
+
+    complete(tasks, 1);
+
+    expect(tasks[1].completed).toBe(true);
+  });
+
+  test('uncompletes a task that is already completed', () => {
+    const tasks = [
+      { description: 'Task 1', completed: true, index: 1 },
+      { description: 'Task 2', completed: false, index: 2 },
+      { description: 'Task 3', completed: false, index: 3 },
+    ];
+
+    complete(tasks, 0);
+
+    expect(tasks[0].completed).toBe(false);
+  });
+});
+
+describe('edit function', () => {
+  test('edits the description of a task', () => {
+    const tasks = [
+      { description: 'Task 1', completed: false, index: 1 },
+      { description: 'Task 2', completed: false, index: 2 },
+      { description: 'Task 3', completed: false, index: 3 },
+    ];
+
+    edit(tasks, 1, 'New task description');
+
+    expect(tasks[1].description).toBe('New task description');
+  });
+});
+
+describe('clearCompleted function', () => {
+  test('removes all completed tasks from the array', () => {
+    const tasks = [
+      { description: 'Task 1', completed: true, index: 1 },
+      { description: 'Task 2', completed: false, index: 2 },
+      { description: 'Task 3', completed: true, index: 3 },
+    ];
+
+    clearCompleted(tasks);
+
+    expect(tasks).toEqual([
+      { description: 'Task 2', completed: false, index: 1 },
+    ]);
+  });
+
+  test('updates the index property of remaining tasks in the array', () => {
+    const tasks = [
+      { description: 'Task 1', completed: true, index: 1 },
+      { description: 'Task 2', completed: false, index: 2 },
+      { description: 'Task 3', completed: true, index: 3 },
+    ];
+
+    clearCompleted(tasks);
+
+    expect(tasks[0].index).toBe(1);
   });
 });
