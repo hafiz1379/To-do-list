dcb0cf136e3e8fb29eadecb5c82fe682b122526b
Gardimy Charles <gardimycharles889@gmail.com>
2023-05-17 22:41:38 -0400

    Fix eslint error

--- src/remove.js
+++ src/remove.js
@@ -1,14 +1,9 @@
 export default (indexToRemove) => {
   const tasks = JSON.parse(localStorage.getItem('tasks'));
-
-  // Find the index of the task to remove
   const taskIndex = tasks.findIndex((task) => task.index === indexToRemove);
 
-  // Remove the task from the array
   if (taskIndex !== -1) {
     tasks.splice(taskIndex, 1);
-
-    // Update the indexes of the remaining tasks
     tasks.forEach((task, index) => {
       task.index = index + 1;
     });
